c5c024d2fd427725fd8b941075bef50146aa17bc
clicktronix <s28.morgan@gmail.com>
2020-05-05 23:04:19 +0600

    Trial version of the bot

---
+++ init.py
@@ -0,0 +1,20 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('token', 'TOKEN')
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
