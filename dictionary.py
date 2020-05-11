be4ac9a1086921918c61923d4f143d829a2afdbf
clicktronix <s28.morgan@gmail.com>
2020-05-11 12:02:24 +0600

    Created vasee realization of bot

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+task_dictionary = (
+    "Wash this cup",
+    "Make bed",
+    "Do 10 squats",
+    "Straighten your back",
+)
--- init.py
+++ init.py
@@ -1,8 +1,11 @@
 from os import getenv
-from telebot import types
+from telebot import TeleBot, types, logger
 from dotenv import load_dotenv
-from telebot import TeleBot
+from dictionary import task_dictionary
+from random import choice
+import logging
 
+logger.setLevel(logging.DEBUG)
 load_dotenv()
 
 token = getenv("token", "TOKEN")
@@ -11,15 +14,39 @@ bot = TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.reply_to(
-        message,
-        "Hello, welcome to the easy routine task bot. \
-        We will send simple daily tasks, good luck and \
-        enjoyment in their implementation.",
+    keyboard = types.InlineKeyboardMarkup()
+    send_task_button = types.InlineKeyboardButton(
+        text="Get task", callback_data="get-task"
     )
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    markup.add(types.InlineKeyboardButton("Get task"))
+    keyboard.add(send_task_button)
+    bot.send_message(
+        message.chat.id,
+        "Hello, I will send simple daily tasks for you",
+        reply_markup=keyboard,
+    )
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data == "get-task":
+        get_task(call.message)
+    elif call.data == "done":
+        task_done(call.message)
+    else:
+        return
+
+
+def get_task(message):
+    task = str(choice(task_dictionary))
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="Done", callback_data="done")
+    keyboard.add(callback_button)
+    bot.send_message(message.chat.id, text=task, reply_markup=keyboard)
+
+
+def task_done(message):
+    bot.send_message(message.chat.id, "You are awesome")
 
 
 if __name__ == "__main__":
-    bot.infinity_polling()
+    bot.polling()
